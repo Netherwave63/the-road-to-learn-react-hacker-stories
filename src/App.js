0334ddee954b5e78ca7de00891108d7c4d338f04
Ethan Chew <YZ.chew@doo.hk>
2020-03-12 13:07:24 +0800

    Complete: Callback Handlers in JSX

--- src/App.js
+++ src/App.js
@@ -20,16 +20,33 @@ const App = () => {
     }
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState('');
-
-  const handleChange = event => {
-    setSearchTerm(event.target.value);
+  const handleSearch = event => {
+    console.log(event.target.value);
   }
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
+      <Search onSearch={handleSearch} />
+
+      <hr />
+
+      <List list={stories} />
+    </div>
+  );
+}
+
+const Search = props => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+    props.onSearch(event);
+  }
+
+  return (
+    <div>
       <label htmlFor='search'>Search: </label>
       <input
         id='search'
@@ -40,12 +57,8 @@ const App = () => {
       <p>
         Searching for <strong>{searchTerm}</strong>.
       </p>
-
-      <hr />
-
-      <List list={stories} />
     </div>
-  );
+  )
 }
 
 const List = props => {
