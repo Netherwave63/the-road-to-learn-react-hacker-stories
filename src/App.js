41abeadc8134acb7de0c41bf2abe50e1753036d8
Ethan Chew <YZ.chew@doo.hk>
2020-03-12 12:51:06 +0800

    Complete: Lists in React

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import React from 'react';
 
-const title = 'React';
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 3,
+    points: 4,
+    objectID: 1,
+  }
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>Hello React</h1>
 
       <label htmlFor='search'>Search: </label>
       <input id='search' type='text' />
+
+      <hr />
+
+      {list.map(item =>
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url} target='_blank'>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </div>
+      )}
     </div>
   );
 }
